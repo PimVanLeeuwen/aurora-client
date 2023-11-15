64a5148b810416a3721db84af9511e7d29411591
Gijsdeman <github@deman.dev>
2023-11-16 00:04:26 +0100

    Rudimentary API example

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Routes from './Routes';
+import { Client, Information } from './api/Client';
 
 export default function App() {
+
+  const [information, setInformation] = useState<Information>();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        let client = new Client();
+        setInformation(await client.getInformation());
+        setLoading(false);
+      } catch (err) {
+        setError(err);
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <h1> Loading </h1>
+    );
+  }
+
+  if (error) {
+    return (
+      <>
+        <h1> Error </h1>
+        <p> {error.message} </p>
+      </>
+    );
+  }
+
   return (
-    <>
-      <h1> Testing </h1>
-      <Routes />
-    </>
+  <>
+    <h1> Example data </h1>
+    <div>
+      {information.id}
+    </div>
+    <Routes/>
+  </>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  server: {
+    port: 8081,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+      }
+    }
+  },
   build: {
     outDir: './build'
   },
-  server: {
-    port: 3002,
-  }
 })
