7f9e4b87daadab2019f25868376d8f51b692de76
Gijsdeman <github@deman.dev>
2023-11-19 15:41:21 +0100

    Add spotify page

---
+++ src/handlers/spotify/components/Background.tsx
@@ -0,0 +1,9 @@
+import styles from '../spotify.module.css';
+
+export default function Background() {
+  return (
+    <div className="h-screen w-full top-0 left-0 absolute -z-10" >
+      <div className={styles.gradient}/>
+    </div>
+  );
+}
--- src/handlers/spotify/index.tsx
+++ src/handlers/spotify/index.tsx
@@ -1,5 +1,22 @@
+import { useState } from 'react';
+import Background from './components/Background';
+
 export default function View() {
+
+  const [albumCover, setAlbumCover] = useState<string>('https://placekitten.com/g/200/200');
+  const [artist, setArtists] = useState<string>('Roy Kakkenberg, Gijs de Man & Samuel Oosterholt');
+  const [song, setSong] = useState<string>('Wie dit leest, trekt een bak!');
+
   return (
-    <h1> Now in spotify view </h1>
+    <>
+      <div className="h-screen flex items-center justify-center z-10">
+        <img className="h-1/2 mr-6" src={albumCover}/>
+        <div className="w-fit max-w-4xl flex flex-col justify-center">
+          <p className="text-black text-6xl p-4 font-bold">{artist}</p>
+          <p className="text-white text-6xl p-4 font-semibold">{song}</p>
+        </div>
+      </div>
+      <Background/>
+    </>
   );
 }
