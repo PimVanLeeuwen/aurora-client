872a94cc5d534215d52810f1c268039c35798626
Roy Kakkenberg <me@roykakkenberg.nl>
2024-06-19 18:05:08 +0200

    fix: no track playing when starting CurrentlyPlayingTrackHandler

--- src/handlers/spotify/index.tsx
+++ src/handlers/spotify/index.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import React from 'react';
 import { Socket } from 'socket.io-client';
 import BlurredImage from '../stage-effects/components/backgrounds/BlurredImage';
+import { SpotifyService } from '../../api';
 
 interface Props {
   socket: Socket;
@@ -18,12 +19,18 @@ export default function SpotifyView({ socket }: Props) {
   const [artist, setArtists] = useState<string>('Roy Kakkenberg, Gijs de Man & Samuel Oosterholt');
   const [song, setSong] = useState<string>('Wie dit leest, trekt een bak!');
 
+  const handleTrackChange = (trackChangeEvent: TrackChangeEvent[]) => {
+    setArtists(trackChangeEvent[0].artists.join(', '));
+    setSong(trackChangeEvent[0].title);
+    setAlbumCover(trackChangeEvent[0].cover);
+  };
+
   React.useEffect(() => {
+    SpotifyService.getSpotifyCurrentlyPlaying().then(handleTrackChange);
+
     socket.on('change_track', (event: any[]) => {
-      const trackChangeEvent = event[0] as TrackChangeEvent[];
-      setArtists(trackChangeEvent[0].artists.join(', '));
-      setSong(trackChangeEvent[0].title);
-      setAlbumCover(trackChangeEvent[0].cover);
+      const trackChangeEvents = event[0] as TrackChangeEvent[];
+      handleTrackChange(trackChangeEvents);
     });
 
     return () => {
@@ -38,16 +45,16 @@ export default function SpotifyView({ socket }: Props) {
         <img className="h-1/2 mr-6" src={albumCover} />
         <div className="w-fit max-w-4xl flex flex-col justify-center font-raleway">
           <p
-            className="text-black text-6xl p-4 font-bold shadow-gray-200 drop-shadow-xl"
-            style={{ filter: 'drop-shadow(0 0 5px rgb(255 255 255 /0.7))' }}
-          >
-            {artist}
-          </p>
-          <p
             className="text-white text-6xl p-4 font-semibold font-raleway"
             style={{ filter: 'drop-shadow(0 0 5px rgb(0 0 0 /0.4))' }}
           >
             {song}
+          </p>
+          <p
+            className="text-white text-5xl italic p-4 font-bold shadow-gray-200 drop-shadow-xl"
+            style={{ filter: 'drop-shadow(0 0 5px rgb(0 0 0 /0.4))' }}
+          >
+            {artist}
           </p>
         </div>
       </div>
