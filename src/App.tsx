f990568dfeb884622cb56f3aee5ede716f0895bb
Gijsdeman <github@deman.dev>
2024-11-10 22:37:28 +0100

    fix(api): use correct base route

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,13 @@ import { BrowserRouter } from 'react-router-dom';
 import AuthContextProvider from './contexts/AuthContext';
 import HandlerSwitcher from './HandlerSwitcher';
 import ErrorBoundary from './ErrorBoundary';
+import { client } from './api';
 
 export default function App() {
+  client.setConfig({
+    baseUrl: '/api',
+  });
+
   return (
     <ErrorBoundary>
       <BrowserRouter>
